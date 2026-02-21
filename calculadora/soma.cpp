83b4b01208ee4a16491e3e965d04ad4ff2900d6c
luiza-k <lkenchicoski@gmail.com>
2026-02-21 14:59:16 +0000

    exercícios 1

---
+++ calculadora/soma.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void){
+
+    int a, b, soma;
+    cout << " Quais os números que você quer somar? ";
+    cin >> a >> b ;
+
+   
+    soma =  a + b ;
+
+    cout << "A soma dos numeros intregues é: "<< (a + b ) << "/n";
+
+
+    return 0;
+
+}
---
+++ hello/hello.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void){
+    string nome, sobrenome;
+    cout << " Qual seu nome e sobrenome?";
+    cin >> nome >> sobrenome;
+    cout << "olá, "<< nome <<" "<< sobrenome << "!\n";
+    
+    
+    return 0;
+
+}
